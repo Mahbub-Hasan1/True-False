777b7fa6492d2e0f81f542d26b4faa1b189c618c
Md-Mahbub-Ahmed <67556397+Md-Mahbub-Ahmed@users.noreply.github.com>
2020-08-07 10:32:09 +0600

    True-False

---
+++ True-false.js
@@ -0,0 +1,22 @@
+//Falsy:
+// 0
+// ""
+// false
+// undefined
+// null
+// NaN
+
+//Truthy:
+// '0'
+// ' '
+// []
+// {}
+
+
+let name = 12;
+if (name || name == 0) {
+    console.log("condition is true");
+} 
+else {
+    console.log("condition is false");
+}
